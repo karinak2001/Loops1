f4d8438ada7e3a2aa72bc603341e947bacd3aec3
Karina Kiryanov <karinak2001@edu.aac.ac.il>
2022-11-16 17:54:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}   public static void main(String[] args) {
+    /*for (int x =1; x<=1000; x++) {
+    System.out.println(x);
+   מריץ מספרים מ1 עד 1000   }*
+
+     */
+
+}
+public static void main(String[] args) {
+  Scanner scanner = new scanner (System.in);
+  int number ;
+  int sum = ;
+  do{
+      System.out.println("enter a number");
+  }
+}
